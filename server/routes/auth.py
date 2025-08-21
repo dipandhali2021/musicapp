80075f10164c443d37b9f017eb4e8f713822a0b7
Dipan Dhali <dipandhali2021@gmail.com>
2025-08-22 02:26:02 +0530

    Add user login functionality with validation and error handling

--- server/routes/auth.py
+++ server/routes/auth.py
@@ -1,15 +1,16 @@
-from fastapi import Depends, HTTPException
-from models.user import User
-from pydantic_schema.user_create import UserCreate
 import uuid 
 import bcrypt
-from fastapi import APIRouter
-from sqlalchemy.orm import Session
+from models.user import User
 from database import get_db
+from fastapi import APIRouter
+from fastapi import Depends, HTTPException
+from pydantic_schema.user_create import UserCreate
+from pydantic_schema.user_login import UserLogin
+from sqlalchemy.orm import Session
 
 router = APIRouter()
 
-@router.post('/signup')
+@router.post('/signup',status_code=201)
 def signup_user(user: UserCreate, db: Session = Depends(get_db)):
     # check if the user already exists in the database
     user_db=db.query(User).filter(User.email == user.email).first()
@@ -25,4 +26,13 @@ def signup_user(user: UserCreate, db: Session = Depends(get_db)):
     db.add(user_db)
     db.commit()
     db.refresh(user_db)
+    return user_db
+
+@router.post('/login')
+def login_user(user: UserLogin, db: Session = Depends(get_db)):
+    user_db = db.query(User).filter(User.email == user.email).first()
+    if  not user_db:
+        raise HTTPException(400,'User with this email does not exist')
+    if not bcrypt.checkpw(user.password.encode(), user_db.password):
+        raise HTTPException(status_code=400, detail="Invalid email or password")
     return user_db
